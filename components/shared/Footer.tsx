24cf8a17ccbc89ad5c829f2ef526bd80af64ad42
CZEJAY <calebjimmy67@gmail.com>
2024-04-29 16:35:16 +0100

    updating

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -3,18 +3,18 @@ import Link from "next/link"
 
 const Footer = () => {
   return (
-    <footer className="border-t">
-      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+    <footer className="border-t w-full max-h-20 flex items-center justify-center pt-14">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-1 text-center sm:flex-row">
         <Link href='/'>
           <Image 
-            src="/assets/images/logo.svg"
+            src="/assets/images/logo1.svg"
             alt="logo"
             width={128}
             height={38}
           />
         </Link>
 
-        <p>2023 Evently. All Rights reserved.</p>
+        <p>2023 Event Tree. All Rights reserved.</p>
       </div>
     </footer>
   )
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -7,11 +7,11 @@ import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
-    <header className="w-full border-b">
+    <header className="w-full max-h-24 border-b flex items-center justify-center">
       <div className="wrapper flex items-center justify-between">
-        <Link href="/" className="w-36">
+        <Link href="/" className="w-40">
           <Image 
-            src="/assets/images/logo.svg" width={128} height={38}
+            src="/assets/images/logo1.svg" width={190} height={68}
             alt="Evently logo" 
           />
         </Link>
